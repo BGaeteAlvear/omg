70c8a56b5b4cf5e07652946946b33013938fdfba
Brian Gaete Alvear <b.gaete.digilab@gmail.com>
2019-08-10 01:04:14 -0400

    icons

--- wp-content/themes/omg/page-private-labels.php
+++ wp-content/themes/omg/page-private-labels.php
@@ -107,102 +107,87 @@ Template Name: Products Private Labels
                         $taxonomy = 'category';
                         $terms = get_terms($taxonomy);
 
-                        if ($terms && !is_wp_error($terms)) :
-                        ?>
-
+                        if ($terms && !is_wp_error($terms)) :?>
                             <?php foreach ($terms as $term) { ?>
 
-                                <?php if($term->slug !='other-products'){ ?>
-
-                                    <div class="card-header" id="headingOne" style="background-color: <?php $catIMGId = get_field('color', 'term_' . $term->term_id); ?>">
+                                <?php if($term->slug !='sin-categoria'){ ?>
+                                <?php $color = get_field('color', 'term_' . $term->term_id); ?>
+                                    <div class="card-header" id="headingOne" style="background-color: <?php echo $color ?>">
                                         <h2 class="mb-0 white f-700">
-                                            <button class="btn btn-link pull-left white" type="button" data-toggle="collapse" data-target="#elem-1" aria-expanded="true" aria-controls="elem-1">
-                                                Collapsible Group Item #1
+                                            <button class="btn btn-link pull-left white font-18" type="button" data-toggle="collapse" data-target="#<?php echo $term->slug; ?>" aria-expanded="true" aria-controls="<?php echo $term->slug; ?>">
+                                                <?php echo $term->name; ?>
                                             </button>
                                         </h2>
                                     </div>
-
-                                    <div id="elem-1" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                    <?php
+                                    $posts_array = get_posts(
+                                        array(
+                                            'posts_per_page' => -1,
+                                            'post_type' => 'products',
+                                            'tax_query' => array(
+                                                array(
+                                                    'taxonomy' => 'category',
+                                                    'field' => 'term_id',
+                                                    'terms' => $term->term_id,
+                                                )
+                                            )
+                                        )
+                                    ); ?>
+                                    <div id="<?php echo $term->slug; ?>" class="collapse show" aria-labelledby="<?php echo $term->slug; ?>" data-parent="#accordionExample">
                                         <div class="card-body">
                                             <table class="table table-products px-0">
                                                 <thead>
-                                                <tr>
-                                                    <th scope="col">PRODUCTS</th>
-                                                    <th scope="col">COUNTRY OF ORIGIN</th>
-                                                    <th scope="col">ORGANIC</th>
-                                                    <th scope="col">KOSHER</th>
-                                                    <th scope="col">VEGAN</th>
-                                                    <th scope="col">RAW</th>
-                                                </tr>
+                                                    <tr>
+                                                        <th scope="col">PRODUCTS</th>
+                                                        <th scope="col">COUNTRY OF ORIGIN</th>
+                                                        <th scope="col">ORGANIC</th>
+                                                        <th scope="col">KOSHER</th>
+                                                        <th scope="col">VEGAN</th>
+                                                        <th scope="col">RAW</th>
+                                                    </tr>
                                                 </thead>
                                                 <tbody>
-                                                <tr>
-                                                    <td>Mark</td>
-                                                    <td>Otto</td>
-                                                    <td></td>
-                                                    <td></td>
-                                                    <td></td>
-                                                    <td></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Jacob</td>
-                                                    <td>Thornton</td>
-                                                    <td></td>
-                                                    <td></td>
-                                                    <td></td>
-                                                <td></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Larry</td>
-                                                    <td>the Bird</td>
-                                                    <td></td>
-                                                    <td></td>
-                                                    <td></td>
-                                                    <td></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Larry</td>
-                                                    <td>the Bird</td>
-                                                    <td></td>
-                                                    <td></td>
-                                                    <td></td>
-                                                    <td></td>
-                                                </tr>
-                                            </tbody>
+                                                    <?php foreach ($posts_array as $post) { ?>
+
+                                                        <?php if ( have_rows( 'products' ) ) : ?>
+
+                                                            <?php while ( have_rows( 'products' ) ) : the_row(); ?>
+                                                                <tr>
+                                                                    <td><?php the_sub_field( 'name_product' ); ?></td>
+                                                                    <td><?php the_sub_field( 'country_origin' ); ?></td>
+                                                                    <?php // type ( value )
+                                                                    $type_array = get_sub_field( 'type' );
+                                                                    if ( $type_array ):
+                                                                        foreach ( $type_array as $type_item ): ?>
+                                                                            <?php if ($type_item == 'organic'): ?>
+                                                                            <td> <?php echo $type_item ;?><i class="fa fa-circle" style="color :#A9BD5C" aria-hidden="true"></i> </td>
+                                                                            <?php endif; ?>
+                                                                            <?php if ($type_item == 'kosher'): ?>
+                                                                                <td> <?php echo $type_item ;?><i class="fa fa-circle" style="color :#A9BD5C" aria-hidden="true"></i> </td>
+                                                                            <?php endif; ?>
+                                                                            <?php if ($type_item == 'vegan'): ?>
+                                                                                <td> <?php echo $type_item ;?><i class="fa fa-circle" style="color :#A9BD5C" aria-hidden="true"></i> </td>
+                                                                            <?php endif; ?>
+                                                                            <?php if ($type_item == 'raw'): ?>
+                                                                                <td> <?php echo $type_item ;?><i class="fa fa-circle" style="color :#A9BD5C" aria-hidden="true"></i> </td>
+                                                                            <?php endif; ?>
+                                                                        <?php endforeach;
+                                                                    endif; ?>
+                                                                </tr>
+                                                            <?php endwhile; ?>
+
+                                                        <?php else : ?>
+                                                            <?php // no rows found ?>
+                                                        <?php endif; ?>
+                                                    <?php } ?>
+                                                </tbody>
                                             </table>
                                         </div>
                                     </div>
-
                             <?php } ?>
                         <?php } ?>
-                    <?php endif; ?>
-                    <div class="card col-12 pb-2">
-                        <div class="card-header" id="headingTwo">
-                            <h2 class="mb-0">
-                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    Collapsible Group Item #2
-                                </button>
-                            </h2>
-                        </div>
-                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                            <div class="card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card col-12 pb-2">
-                        <div class="card-header" id="headingThree">
-                            <h2 class="mb-0">
-                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                    Collapsible Group Item #3
-                                </button>
-                            </h2>
-                        </div>
-                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                            <div class="card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                            </div>
-                        </div>
+                        <?php endif; ?>
+
                     </div>
                 </div>
             </div>
